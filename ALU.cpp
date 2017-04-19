cf4f96a37bed0b3449c11b209ac97ebf8f0b8a0b
Daniel Simmons <danielsimmons@Daniels-MacBook-Pro.local>
2017-04-19 10:12:50 -0400

    just trying to pull sshit

--- ALU.cpp
+++ ALU.cpp
@@ -7,3 +7,78 @@
 //
 
 #include "ALU.hpp"
+
+string intToHex(int integer);
+
+ALU::ALU() {
+
+}
+
+ALU::ALU(int _operation, int _operand1, int _operand2) {
+
+	//convert binary strings to int
+
+	operation = _operation;
+	operand1 = _operand1;
+	operand2 = _operand2;
+
+	if (operation < 0) {
+		cout << "Error: operation value must be 0-2" << endl;
+		exit();
+	}
+
+	execute();
+}
+
+void ALU::execute() {
+	if (operation == 0) compare();
+
+	else if (operation == 1) add();
+
+	else if (operation == 2) subtract();
+}
+
+/*
+* Compares the two operands, returns 1 if the same and 0 if different
+*/
+int ALU::compare() {
+
+	if (operand1 == operand2) return 1;
+
+	//operands are different
+	return 0;
+}
+
+/*
+* Adds two operands and returns the result
+*/
+string ALU::add() {
+
+	string result = intToHex(operand1 + operand2);
+
+	return result;
+}
+
+/*
+* Subtracts two operands and returns the result
+*/
+string ALU::subtract() {
+	
+	//probably need some binary substraction method
+	//how to represent a negative integer?
+	int result = 0;
+
+	return result;
+}
+
+/*
+* converts int to a string representation of its hex value
+*/
+string intToHex(int integer){
+    string hexString;
+    stringstream ss;
+    integer >> ss;
+    ss >> std::hex >> hexString;
+    return hexString;
+}
+
--- ALU.hpp
+++ ALU.hpp
@@ -10,5 +10,30 @@
 #define ALU_hpp
 
 #include <stdio.h>
+#include "Instruction.hpp"
+#include "Registers.hpp"
+#include "ALU.hpp"
+#include "DataMemory.hpp"
+#include "Instruction.hpp"
+
+
+class ALU {
+
+private:
+	int operation; //0 = compare, 1 = add, 2 = subtract
+	int operand1;
+	int operand2;
+
+	void execute();
+	int compare();
+	string add();
+	string subtract();
+
+public:
+
+	ALU();
+	ALU(int _operation, int _operand1, int _operand2);
+
+};
 
 #endif /* ALU_hpp */
