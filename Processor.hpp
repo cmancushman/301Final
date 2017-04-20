fc7666b613a5ac4437e81d2247d71764fc03b984
Chris <christopher.cushman@richmond.edu>
2017-04-19 20:42:18 -0400

    ALU

--- Processor.hpp
+++ Processor.hpp
@@ -6,13 +6,9 @@
 //  Copyright © 2017 Ubord, Inc. All rights reserved.
 //
 
-<<<<<<< HEAD
-#ifndef Processor.hpp
-#define Processor.hpp
-=======
+
 #ifndef Processor_hpp
 #define Processor_hpp
->>>>>>> 95c998b18517b30ec1c47f3506eec7f82ca8b0ab
 
 #include <stdio.h>
 #include <iostream>
@@ -21,11 +17,8 @@
 #include "ProgramCounter.hpp"
 #include "DataMemory.hpp"
 #include "ALU.hpp"
-<<<<<<< HEAD
-#include "Multiplexor.hpp"
-=======
+
 #include "Multiplexer.hpp"
->>>>>>> 95c998b18517b30ec1c47f3506eec7f82ca8b0ab
 
 class Processor{
 private:
@@ -35,23 +28,7 @@ private:
     Registers registerFile;
     Parser parse;
     ALU registerALU;
-<<<<<<< HEAD
-    
-public:
-    
-    /* Constructor for program counter */
-    ProgramCounter();
-    
-    
-    /* Jump to a given address */
-    void jump(double amount);
-    
-    /* Increment by 4 */
-    void increment();
-    
-    /* Retruns the current address */
-    double getAddress();
-=======
+
     ProgramCounter programCounter;
     
     void fetch();
@@ -65,14 +42,10 @@ public:
     
     Processor();
 
->>>>>>> 95c998b18517b30ec1c47f3506eec7f82ca8b0ab
     
 };
 
 
 
-<<<<<<< HEAD
-#endif /* Processor.hpp */
-=======
+
 #endif /* Processor_hpp */
->>>>>>> 95c998b18517b30ec1c47f3506eec7f82ca8b0ab
