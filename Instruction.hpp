1cba3ac1aac5c71de6a6bb46cc57d8cbb24a0d2c
Alex Christine <Alex@Alexs-MacBook-Pro.local>
2017-04-19 20:32:20 -0400

    Comments

--- Instruction.hpp
+++ Instruction.hpp
@@ -16,23 +16,51 @@ using namespace std;
 class Instruction{
     
 private:
+    
+    // Specifies J, R, or I-Type
     int typeOfInstruction;
+    
+    // Op-field: add, jump, etc.
     string component1;
+    
+    /*
+        compenents hold additional operands for instruction including
+        memory addresses, register names, etc.
+    */
     string component2;
     string component3;
     string component4;
+    
+    // Immediate field if applicable
     string immediate;
+    
+    // Jump component if applicable
     string jumpComponent;
     
 public:
     
+    /* Constructor with all inputs */
     Instruction(string str1, string str2, string str3, string str4);
+    
+    /* Returns instruction type */
     string getType();
+    
+    /* returns component1 */
     string getComponent1();
+    
+    /* Returns component2 */
     string getComponent2();
+    
+    /* Returns component3 */
     string getComponent3();
+    
+    /* Returns component4 */
     string getComponent4();
+    
+    /* Returns immediate field */
     string getImmediate();
+    
+    /* Prints the instruction */
     void print();
 };
 #endif /* Instruction_hpp */
